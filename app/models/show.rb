2cbc561dba9ca098aa03cc2e534b003683a27c86
Lena Hou <lenahou2013@gmail.com>
2021-05-24 14:34:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,12 +4,16 @@ class Show < ActiveRecord::Base
   end
 
 def self.most_popular_show
-  self.where("rating= ?". self.highest_rating)
+  self.where("rating= ?", self.highest_rating)
 end
 
 
 def self.lowest_rating
   self.minimum(:rating)
+end
+
+def self.least_popular_show
+  self.where("rating = ?", self.lowest_rating)
 end
 
 end
