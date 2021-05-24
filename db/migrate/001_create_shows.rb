db165b8550c871aefad9259f9029f1d9e7bbbb6e
Lena Hou <lenahou2013@gmail.com>
2021-05-24 14:29:38 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,8 @@
 class CreateShows < ActiveRecord::Migration[5.2]
-  def change
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
+  create_table :shows do |t|
+    t.string :name
+    t.string :network
+    t.string :day
+    t.integer :rating
   end
-
 end
